56f66aede61a10a17a88ad1cd1678139106629df
xan <47989552+style-x@users.noreply.github.com>
2022-02-27 20:16:45 +0100

    Start Projekt
    Dateien erstellt

---
+++ script.js
@@ -0,0 +1,19 @@
+let myLibrary = [
+  {
+    id: 1,
+    title: 'Die kleine Raupe Nimmersatt',
+    author: 'Eric Carle',
+    nbr_of_pages: 32,
+    publishing_date: 'Juni 3 1969',
+    read_status: true,
+    insertion_date: '02/27/2022, 8:10:58 PM',
+  }
+];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
