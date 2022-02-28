ddec1edecac858803044098de1e4347463165806
xan <47989552+style-x@users.noreply.github.com>
2022-02-28 20:15:06 +0100

    Erster Versuch
    Erste Ausgabe der Buch Vorlage im Grid

--- script.js
+++ script.js
@@ -10,10 +10,27 @@ let myLibrary = [
   }
 ];
 
+let fragment = document.createDocumentFragment();
+const target = document.getElementById('container');
+
 function Book() {
   // the constructor...
-}
+};
 
 function addBookToLibrary() {
   // do stuff here
-}
+};
+
+myLibrary.forEach(element => {
+  let newNode = document.createElement('div');
+  newNode.classList.add('book');
+  let h3 = document.createElement('h3');
+  h3.appendChild(document.createTextNode(`${element.title}`));
+  let p = document.createElement('p');
+  p.appendChild(document.createTextNode(`${element.author} (${element.publishing_date})`));
+  newNode.appendChild(h3);
+  newNode.appendChild(p);
+  fragment.appendChild(newNode);
+});
+
+target.appendChild(fragment);
