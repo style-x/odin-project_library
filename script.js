97a640c03499d16d0aa0ba50af1dff5f70eea1c9
xan <47989552+style-x@users.noreply.github.com>
2022-03-01 20:20:22 +0100

    Regal steht
    Bücher werden nun im Grid angezeigt.
    Delete Button eingebunden.
    Wie nun löschen..?
    (Zufügen fehlt auch noch)

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ let myLibrary = [
     publishing_date: 'Juni 3 1969',
     read_status: true,
     insertion_date: '02/27/2022, 8:10:58 PM',
+  },
+  {
+    id: 2,
+    title: 'Test',
+    author: 'Ich',
+    nbr_of_pages: 15,
+    publishing_date: 'Nov 29 1985',
+    read_status: true,
+    insertion_date: '03/01/2022, 8:01:24 PM',
   }
 ];
 
 let fragment = document.createDocumentFragment();
 const target = document.getElementById('container');
+const eraseBtn = document.getElementsByClassName('erase');
 
 function Book() {
   // the constructor...
@@ -26,11 +36,21 @@ myLibrary.forEach(element => {
   newNode.classList.add('book');
   let h3 = document.createElement('h3');
   h3.appendChild(document.createTextNode(`${element.title}`));
+  let h5 = document.createElement('h5');
+  h5.appendChild(document.createTextNode(`${element.author} (${element.publishing_date})`));
   let p = document.createElement('p');
-  p.appendChild(document.createTextNode(`${element.author} (${element.publishing_date})`));
+  p.classList.add('erase');
+  p.appendChild(document.createTextNode('x'));
   newNode.appendChild(h3);
+  newNode.appendChild(h5);
   newNode.appendChild(p);
   fragment.appendChild(newNode);
 });
 
 target.appendChild(fragment);
+
+eraseBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    
+  })
+})
