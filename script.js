e1f320198848d1b67156ba70c5b3423079705678
xan <47989552+style-x@users.noreply.github.com>
2022-03-06 20:14:07 +0100

    Löscht nur 1..
    Man kann leider nur 1 Buch entfernen, danach nicht mehr...

--- script.js
+++ script.js
@@ -7,47 +7,78 @@ let myLibrary = [
     publishing_date: 'Juni 3 1969',
     read_status: true,
     insertion_date: '02/27/2022, 8:10:58 PM',
+    show: true,
   },
   {
-    id: 2,
+    id: 5,
     title: 'Test',
     author: 'Ich',
     nbr_of_pages: 15,
     publishing_date: 'Nov 29 1985',
     read_status: true,
     insertion_date: '03/01/2022, 8:01:24 PM',
+    show: true,
+  },
+  {
+    id: 18,
+    title: 'Nochmal',
+    author: 'Ich',
+    nbr_of_pages: 999,
+    publishing_date: 'March 6 2022',
+    read_status: true,
+    insertion_date: '03/06/2022, 7:27:56 PM',
+    show: true,
   }
 ];
 
-let fragment = document.createDocumentFragment();
+const fragment = document.createDocumentFragment();
 const target = document.getElementById('container');
-
-function Book() {
-  // the constructor...
-};
-
-function addBookToLibrary() {
-  // do stuff here
-};
-
-myLibrary.forEach(element => {
-  let newNode = document.createElement('div');
-  newNode.classList.add('book');
-  let h3 = document.createElement('h3');
-  h3.appendChild(document.createTextNode(`${element.title}`));
-  let h5 = document.createElement('h5');
-  h5.appendChild(document.createTextNode(`${element.author} (${element.publishing_date})`));
-  let p = document.createElement('p');
-  p.classList.add('erase');
-  p.appendChild(document.createTextNode('x'));
-  newNode.appendChild(h3);
-  newNode.appendChild(h5);
-  newNode.appendChild(p);
-  fragment.appendChild(newNode);
-});
-
-target.appendChild(fragment);
-
+const add = document.getElementById('btn-add');
 const eraseBtn = document.getElementsByClassName('erase');
 
-console.log(eraseBtn);
+function Book() {
+  [...target.childNodes].forEach(el => el.remove());
+  /*while (target.firstChild) {
+    target.firstChild.remove()
+    console.log(target.firstChild);
+  } */
+  myLibrary.forEach(element => {
+    if(element.show) {
+      let newNode = document.createElement('div');
+      newNode.classList.add('book');
+      let h3 = document.createElement('h3');
+      h3.appendChild(document.createTextNode(`${element.title}`));
+      let h5 = document.createElement('h5');
+      h5.appendChild(document.createTextNode(`${element.author} (${element.publishing_date})`));
+      let p = document.createElement('p');
+      p.classList.add('erase');
+      p.appendChild(document.createTextNode('x'));
+      p.setAttribute('data-id', `${element.id}`);
+      newNode.appendChild(h3);
+      newNode.appendChild(h5);
+      newNode.appendChild(p);
+      fragment.appendChild(newNode);
+    } else return;
+    target.appendChild(fragment);
+  });
+};
+
+Book();
+
+Array.from(eraseBtn).forEach(btn => {
+  btn.addEventListener('click', event => {
+    let id = btn.getAttribute('data-id');
+    const erase = myLibrary.filter(book => book.id == id);
+    erase[0].show = false;
+    console.log("gelöscht");
+    Book();
+  })
+});
+
+add.addEventListener('click', event => {
+  addBookToLibrary();
+})
+
+function addBookToLibrary() {
+  console.log("Buch hinzufügen..");
+};
