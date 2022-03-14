6b9d045c7a3db3551373d7632b26eec2c764f8bf
xan <47989552+style-x@users.noreply.github.com>
2022-03-14 20:06:34 +0100

    Summary
    wird einfach nichts.. besser neu anfangen

--- script.js
+++ script.js
@@ -1,68 +1,40 @@
+'use strict'
+
 document.querySelector('#btn-add').addEventListener('click', addBookToLibrary);
 document.querySelector('#btn-display').addEventListener('click', changeDisplay);
 
-let myLibrary = [
-  {
-    id: 1,
-    title: 'Die kleine Raupe Nimmersatt',
-    author: 'Eric Carle',
-    nbr_of_pages: 32,
-    publishing_date: 'Juni 3 1969',
-    read_status: true,
-    insertion_date: '02/27/2022, 8:10:58 PM',
-    show: true,
-  },
-  {
-    id: 5,
-    title: 'Test',
-    author: 'Ich',
-    nbr_of_pages: 15,
-    publishing_date: 'Nov 29 1985',
-    read_status: true,
-    insertion_date: '03/01/2022, 8:01:24 PM',
-    show: true,
-  },
-  {
-    id: 18,
-    title: 'Nochmal',
-    author: 'Ich',
-    nbr_of_pages: 999,
-    publishing_date: 'March 6 2022',
-    read_status: true,
-    insertion_date: '03/06/2022, 7:27:56 PM',
-    show: true,
-  }
-];
+let myLibrary = [];
 
 const fragment = document.createDocumentFragment();
 const target = document.getElementById('container');
+const book = document.querySelectorAll('.book');
 const eraseBtn = document.getElementsByClassName('erase');
 
 function build() {
-  [...target.childNodes].forEach(el => el.remove());
-  myLibrary.forEach(element => {
-    if(element.show) {
-      let newNode = document.createElement('div');
-      newNode.classList.add('book');
-      let h3 = document.createElement('h3');
-      h3.appendChild(document.createTextNode(`${element.title}`));
-      let h5 = document.createElement('h5');
-      h5.appendChild(document.createTextNode(`${element.author} (${element.publishing_date})`));
-      let p = document.createElement('p');
-      p.classList.add('erase');
-      p.appendChild(document.createTextNode('x'));
-      p.setAttribute('data-id', `${element.id}`);
-      newNode.appendChild(h3);
-      newNode.appendChild(h5);
-      newNode.appendChild(p);
-      fragment.appendChild(newNode);
-    } else return;
-    target.appendChild(fragment);
-  });
+  book.forEach(book => target.removeChild(book));
+  for (let i = 0; i < myLibrary.length; i++){
+      createBook(myLibrary[i]);
+  };
 };
 
-build();
+function createBook(item) {
+  let newNode = document.createElement('div');
+  newNode.classList.add('book');
+  let h3 = document.createElement('h3');
+  h3.appendChild(document.createTextNode(`${item.title}`));
+  let h5 = document.createElement('h5');
+  h5.appendChild(document.createTextNode(`${item.author} (${item.publishing_date})`));
+  let p = document.createElement('p');
+  p.classList.add('erase');
+  p.appendChild(document.createTextNode('x'));
+  p.setAttribute('data-id', `${item.id}`);
+  newNode.appendChild(h3);
+  newNode.appendChild(h5);
+  newNode.appendChild(p);
+  fragment.appendChild(newNode);
+}
 
+/*
 Array.from(eraseBtn).forEach(btn => {
   btn.addEventListener('click', event => {
     let id = btn.getAttribute('data-id');
@@ -72,6 +44,7 @@ Array.from(eraseBtn).forEach(btn => {
     build();
   })
 });
+*/
 
 function addBookToLibrary() {
   console.log("Buch hinzufügen..");
@@ -92,8 +65,10 @@ function rebuild() {
       let data = localStorage.getItem('myLibrary') 
       myLibrary = JSON.parse(data);
       build();
+      console.log("Daten aus LocalStorage geladen..");
   }else {
       build();
+      console.log("Keine Daten gefunden..");
   }
 }
 
