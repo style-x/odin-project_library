4414359219a7d24d370429b9f42ce106dab65aa8
xan <47989552+style-x@users.noreply.github.com>
2022-03-18 20:17:59 +0100

    Löschen funktioniert!
    VORSICHT, wird im Cache gespeichert...

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const book = document.querySelectorAll('.book');
 const eraseBtn = document.getElementsByClassName('erase');
 
 function build() {
-  book.forEach(book => target.removeChild(book));
+  while (target.firstChild) {
+    target.removeChild(target.lastChild);
+  }
   for (let i = 0; i < myLibrary.length; i++){
       createBook(myLibrary[i]);
   };
@@ -28,48 +30,54 @@ function createBook(item) {
   p.classList.add('erase');
   p.appendChild(document.createTextNode('x'));
   p.setAttribute('data-id', `${item.id}`);
+  p.addEventListener('click', event => {
+    let id = item.id;
+    let newLibrary = myLibrary.filter(item => item.id !== id);
+    console.log("gelöscht");
+    myLibrary = newLibrary;
+    setData()
+    build();
+  });
   newNode.appendChild(h3);
   newNode.appendChild(h5);
   newNode.appendChild(p);
   fragment.appendChild(newNode);
-}
-
-/*
-Array.from(eraseBtn).forEach(btn => {
-  btn.addEventListener('click', event => {
-    let id = btn.getAttribute('data-id');
-    const erase = myLibrary.filter(book => book.id == id);
-    erase[0].show = false;
-    console.log("gelöscht");
-    build();
-  })
-});
-*/
+  target.appendChild(fragment);
+};
 
 function addBookToLibrary() {
-  console.log("Buch hinzufügen..");
+  console.log("Modal öffnen");
+  
 };
 
 function changeDisplay() {
   let display = document.getElementById("container");
-  display.classList.toggle("displayCards")
+  display.classList.toggle("displayCards");
   console.log("Change Display..");
-}
+};
 
 function setData() {
   localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
-}
+};
 
 function rebuild() {
   if(localStorage.myLibrary) {
-      let data = localStorage.getItem('myLibrary') 
+      let data = localStorage.getItem('myLibrary');
       myLibrary = JSON.parse(data);
       build();
       console.log("Daten aus LocalStorage geladen..");
   }else {
       build();
       console.log("Keine Daten gefunden..");
-  }
-}
+  };
+};
+
+const getBookFromInput = () => {
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const isRead = document.getElementById('isRead').checked
+  return new Book(title, author, pages, isRead)
+};
 
 rebuild();
